ad5aa4851a17def779fa0b18b5fc547d5404bd0c
rosejjz <rosejjz@gmail.com>
2023-03-02 17:47:49 -0800

    added photo feature

--- main_app/views.py
+++ main_app/views.py
@@ -1,20 +1,107 @@
-from django.shortcuts import render
-from .models import Finch
+from django.shortcuts import render, redirect
+from .models import Finch, Toy, Photo
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic import ListView, DetailView
+from .form import FeedingForm
+import uuid
+import boto3
+import os
+
 
 # Create your views here.
+
 
 def home(request):
     return render(request, 'home.html')
 
+
 def about(request):
     return render(request, 'about.html')
+
 
 def finches_index(request):
     finches = Finch.objects.all()
     return render(request, 'finches/index.html', {
-        'finches': finches
+        'finches': finches,
     })
+
 
 def detail(request, finch_id):
     finch = Finch.objects.get(id=finch_id)
-    return render(request, 'finches/detail.html', {'finch': finch})
+    id_list = finch.toys.all().values_list('id')
+    toys_cat_doesnt_have = Toy.objects.exclude(id__in=id_list)
+    feeding_form = FeedingForm()
+    return render(request, 'finches/detail.html', {
+        'finch': finch,
+        'feeding_form': feeding_form,
+        'toys':toys_cat_doesnt_have
+    })
+
+
+class FinchCreate(CreateView):
+    model = Finch
+    fields = ['name', 'species', 'description', 'age']
+
+
+class FinchUpdate(UpdateView):
+    model = Finch
+    fields = ['species', 'description', 'age']
+
+
+class FinchDelete(DeleteView):
+    model = Finch
+    success_url = '/finches'
+
+
+def add_feeding(request, finch_id):
+    form = FeedingForm(request.POST)
+    if form.is_valid():
+        new_feeding = form.save(commit=False)
+        new_feeding.finch_id = finch_id
+        new_feeding.save()
+    return redirect('detail', finch_id=finch_id)
+
+class ToyCreate(CreateView):
+    model = Toy
+    fields = '__all__'
+
+class ToyList(ListView):
+    model = Toy
+
+class ToyDetail(DetailView):
+    model = Toy
+
+class ToyDelete(DeleteView):
+    model = Toy
+    success_url = '/toys'
+
+class ToyUpdate(UpdateView):
+    model = Toy
+    fields = ['name', 'color']
+
+def assoc_toy(request, finch_id, toy_id):
+  Finch.objects.get(id=finch_id).toys.add(toy_id)
+  return redirect('detail', finch_id=finch_id)
+
+def unassoc_toy(request, finch_id, toy_id):
+  Finch.objects.get(id=finch_id).toys.remove(toy_id)
+  return redirect('detail', finch_id=finch_id)
+
+def add_photo(request, finch_id):
+  #photo-file maps to the "name" str on the <input>
+  photo_file = request.FILES.get('photo-file', None)
+  if photo_file:
+    s3 = boto3.client('s3')
+    # Need a unique "key" (filename) it needs to keep the same file 
+    # extension of the file that was uploaded
+    key = uuid.uuid4().hex[:6] + photo_file.name[photo_file.name.rfind('.'):]
+    try:
+      bucket = os.environ['S3_BUCKET']
+      s3.upload_fileobj(photo_file, bucket, key)
+      url = f"{os.environ['S3_BASE_URL']}{bucket}/{key}"
+      Photo.objects.create(url=url, cat_id=finch_id)
+    except Exception as e:
+      print('An error occurred uploading file to S3')
+      print(e)
+
+  return redirect('detail', finch_id=finch_id)
